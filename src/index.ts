5d14c9fa0dbfe58f7aa4277a61ee035bf904c4db
Kevin Valsted <kvt@kevins-mbp.addwish.com>
2021-09-06 11:49:41 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const github_token : string = core.getInput('my_github_token');
-
+console.log("starting");
 if (github_token.length) {
     core.setOutput("result", "Got the following github token: ", github_token);
 } else {
