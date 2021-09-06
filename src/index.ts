c77b3e09bc7266cd33d1426014a7115288927b48
Kevin Valsted <kvt@kevins-mbp.addwish.com>
2021-09-06 10:46:07 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const github_token : string = core.getInput('my_github_token');
+
+if (github_token.length) {
+    core.setOutput("result", "Got the following github token: ", github_token);
+} else {
+    core.setOutput("result", "Was unable to validate github token");
+}
